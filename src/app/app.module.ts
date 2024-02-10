b00819dd7c3d5735e6dbb49872846236934b12f3
Jesús Villalón <villalongallardo92@gmail.com>
2024-02-10 21:52:23 +0100

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PagesComponent } from './pages/pages.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -18,13 +19,14 @@ import { HeaderComponent } from './shared/header/header.component';
     RegisterComponent,
     NopagefoundComponent,
     DashboardComponent,
-    BreadcrumbsComponent,
+    PagesComponent,
+    HeaderComponent,
     SidebarComponent,
-    HeaderComponent
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   bootstrap: [AppComponent]
 })
